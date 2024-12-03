39a55771f9f8f783a8430238bd18ad71cce4a63b
Kris1027 <obarzanek.work@gmail.com>
2024-12-03 17:50:15 +0100

    build create task api endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,35 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, (req, res) => {
+app.use(express.json());
+app.use(cors());
+
+let tasks = [];
+
+app.post('/api/tasks/create', (req, res) => {
+    try {
+        const { taskInput } = req.body;
+        if (!taskInput || taskInput.trim() === '')
+            return res.status(200).json({ message: 'Task input cannot be empty' });
+
+        const newTask = {
+            id: Math.random(),
+            text: taskInput,
+            complete: false,
+        };
+
+        tasks.push(newTask);
+
+        res.status(201).json({ message: 'Task created successfully', newTask });
+    } catch (error) {
+        console.error('Error in createTask controller', error.message);
+        res.status(500).json({ message: 'Internal Server Error', error: error.message });
+    }
+});
+
+app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
